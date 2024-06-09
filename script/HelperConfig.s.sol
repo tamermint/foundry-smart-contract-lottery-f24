f134df30aa0ff4942e985dcb1e2f6dcca17393cd
Vivek <vivek.mitra@outlook.com>
2024-06-09 19:29:03 +1000

    key hash modified

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -43,7 +43,7 @@ contract HelperConfig is Script {
                 entranceFee: 0.01 ether,
                 interval: 30,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
-                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 10741,
                 callbackGasLimit: 500000,
                 link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
@@ -71,7 +71,7 @@ contract HelperConfig is Script {
                 entranceFee: 0.01 ether,
                 interval: 30,
                 vrfCoordinator: address(vrfCoordinatorV2Mock),
-                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 0, //script will add this
                 callbackGasLimit: 500000,
                 link: address(link),
